7401284a671bfa729ee0d4ae8af040b4d51773ac
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-12 19:23:50 +0300

    add openiddict

--- src/OpeniddictSample.IdentityProviderApi/Program.cs
+++ src/OpeniddictSample.IdentityProviderApi/Program.cs
@@ -1,3 +1,6 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenIddict()
+                .AddServer();
+
 WebApplication app = builder.Build();
 app.Run();
