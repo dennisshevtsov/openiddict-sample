752cd1d9d1b316640b332c4cfec686ca6c26d88f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-12 19:41:27 +0300

    configure ef

--- src/OpeniddictSample.IdentityProviderApi/Program.cs
+++ src/OpeniddictSample.IdentityProviderApi/Program.cs
@@ -1,17 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<DbContext>(options => options.UseOpenIddict());
-builder.Services.AddOpenIddict()
-                .AddCore(options => options.UseEntityFrameworkCore())
-                .AddServer(options =>
+
+builder.Services.AddDbContext<DbContext>(builder =>
                 {
-                  options.SetTokenEndpointUris("connect/token");
-                  options.AllowClientCredentialsFlow();
-                  options.UseAspNetCore()
+                  builder.UseNpgsql("");
+                  builder.UseOpenIddict();
+                });
+builder.Services.AddOpenIddict()
+                .AddCore(builder => builder.UseEntityFrameworkCore())
+                .AddServer(builder =>
+                {
+                  builder.SetTokenEndpointUris("connect/token");
+                  builder.AllowClientCredentialsFlow();
+                  builder.UseAspNetCore()
                          .EnableTokenEndpointPassthrough();
                 })
-                .AddValidation(options => options.UseAspNetCore());
+                .AddValidation(builder => builder.UseAspNetCore());
 
 WebApplication app = builder.Build();
 app.Run();
