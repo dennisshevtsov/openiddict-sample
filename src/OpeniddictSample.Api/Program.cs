6772af2a5dac9669e6b13841e565acb354630a2d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-16 15:29:47 +0300

    add copyright

--- src/OpeniddictSample.Api/Program.cs
+++ src/OpeniddictSample.Api/Program.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 app.Run();
