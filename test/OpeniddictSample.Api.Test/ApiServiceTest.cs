f0e5687098e025b472a828659821f8ee65f77a40
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-22 23:25:56 +0300

    add openiddict client

--- test/OpeniddictSample.Api.Test/ApiServiceTest.cs
+++ test/OpeniddictSample.Api.Test/ApiServiceTest.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Microsoft.Extensions.DependencyInjection;
+using OpenIddict.Client;
 using System.Net;
 
 namespace OpeniddictSample.Api.Test;
@@ -11,6 +13,8 @@ public sealed class SurveyTest
 {
 #pragma warning disable CS8618
   private HttpClient _httpClient;
+  private IServiceScope _serviceScope;
+  private OpenIddictClientService _openIddictClientService;
 #pragma warning restore CS8618
 
   [TestInitialize]
@@ -20,11 +24,24 @@ public sealed class SurveyTest
     {
       BaseAddress = new Uri("http://localhost:5001/api/")
     };
+
+    ServiceCollection services = new();
+    services.AddOpenIddict()
+            .AddClient(options =>
+            {
+              options.AddRegistration(new OpenIddictClientRegistration
+              {
+                Issuer = new Uri("http://localhost:5002"),
+                ClientId = "openiddict-api",
+                ClientSecret = "test",
+              });
+            });
   }
 
   [TestCleanup] public void Cleanup()
   {
     _httpClient?.Dispose();
+    _serviceScope.Dispose();
   }
 
   [TestMethod]
