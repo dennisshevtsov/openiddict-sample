3c98844338629fae428d262195a2b796851de81d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-26 18:17:41 +0300

    add echo

---
+++ src/OpeniddictSample.Api/EchoController.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpeniddictSample.Api;
+
+[Route("api/echo")]
+[ApiController]
+public sealed class EchoController : ControllerBase
+{
+  [HttpGet]
+  [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+  public IActionResult GetSurveys(EchoDto echoDto) => Ok(echoDto);
+}
