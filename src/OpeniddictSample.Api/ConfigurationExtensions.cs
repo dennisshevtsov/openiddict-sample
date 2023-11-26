9d34a8bc033d32ef4de74914e46babd27aa9516a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-26 18:10:56 +0300

    configure auth settings

--- src/OpeniddictSample.Api/ConfigurationExtensions.cs
+++ src/OpeniddictSample.Api/ConfigurationExtensions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Extensions.Configuration;
 
 public static class ConfigurationExtensions
 {
-  public static AuthenticationSettings GetAuthenticationSettings(this IConfiguration configuration) =>
-    configuration.GetSection("API_SERVICE").Get<AuthenticationSettings>() ??
+  public static AuthenticationSettings GetAuthenticationSettings(this IConfiguration configuration, string section = "AUTHENTICATION") =>
+    configuration.GetSection(section).Get<AuthenticationSettings>() ??
     throw new Exception("There are no authenctication settings.");
 }
--- src/OpeniddictSample.Api/Program.cs
+++ src/OpeniddictSample.Api/Program.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using Microsoft.Extensions.Configuration;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.SetUpApi();
 builder.Services.SetUpAuthentication(builder.Configuration.GetAuthenticationSettings());
