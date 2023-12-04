47cebf22439d942c201c698f91e3b75740dfc702
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-04 22:45:03 +0300

    add signin component

--- src/openiddict-sample-client/src/app/app.module.ts
+++ src/openiddict-sample-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './app/components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
