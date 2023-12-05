1af026cf04e9df4b6d42fd9a67d8d4807044069f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-05 22:38:40 +0300

    add modules

--- src/openiddict-sample-client/src/app/app.module.ts
+++ src/openiddict-sample-client/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule            } from '@angular/core';
+import { HttpClientModule    } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule       } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { SigninComponent } from './components/signin/signin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent     } from './app.component';
+import { SigninComponent  } from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
