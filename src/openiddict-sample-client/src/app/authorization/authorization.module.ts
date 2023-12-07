45af35908273a330c0b8902cda702c2478f8b10f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-07 22:23:36 +0300

    impl auth module

--- src/openiddict-sample-client/src/app/authorization/authorization.module.ts
+++ src/openiddict-sample-client/src/app/authorization/authorization.module.ts
@@ -1,10 +1,48 @@
-import { NgModule     } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ModuleWithProviders } from '@angular/core';
+import { NgModule            } from '@angular/core';
+
+import { CommonModule      } from '@angular/common';
+import { HttpClientModule  } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { UserManager } from 'oidc-client';
+
+import { AuthorizationRoutingModule } from './authorization-routing.module';
+import { AuthorizationInterceptor   } from './interceptors';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule,
+    AuthorizationRoutingModule,
+  ],
 })
-export class AuthorizationModule { }
+export class AuthorizationModule {
+  public static forRoot(
+    identityApiUrl: string, appUrl: string, clientId: string, scope: string)
+    : ModuleWithProviders<AuthorizationModule> {
+    return {
+      ngModule: AuthorizationModule,
+      providers: [
+        {
+          provide : HTTP_INTERCEPTORS,
+          useClass: AuthorizationInterceptor,
+          multi   : true,
+        },
+        {
+          provide: UserManager,
+          useFactory: () => new UserManager({
+            authority               : identityApiUrl,
+            client_id               : clientId,
+            redirect_uri            : `${appUrl}/signin-callback`,
+            silent_redirect_uri     : `${appUrl}/silent-callback`,
+            post_logout_redirect_uri: appUrl,
+            response_type           : 'code',
+            scope                   : `openid profile ${scope}`,
+          }),
+        },
+      ],
+    };
+  }
+}
