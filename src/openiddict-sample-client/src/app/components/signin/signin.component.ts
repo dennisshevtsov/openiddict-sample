7ae6d8afbeff2f509b7da1ecfe162bddca502620
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-09 20:53:20 +0300

    implement signin component

--- src/openiddict-sample-client/src/app/components/signin/signin.component.ts
+++ src/openiddict-sample-client/src/app/components/signin/signin.component.ts
@@ -1,10 +1,90 @@
+import { DOCUMENT  } from '@angular/common';
+
 import { Component } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { Inject    } from '@angular/core';
+import { OnInit    } from '@angular/core';
+
+import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup   } from '@angular/forms';
+import { Validators  } from '@angular/forms';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { SigninProps     } from './signin.view-model';
+import { SigninViewModel } from './signin.view-model';
+
+type SigninFormScheme = {
+  [K in keyof SigninProps]: FormControl<SigninProps[K] | null>;
+}
 
 @Component({
-  selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  providers: [
+    SigninViewModel,
+    {
+      provide: Subscription,
+      useFactory: () => new Subscription(),
+    },
+  ],
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit, OnDestroy {
+  private returnUrlValue: undefined | string;
+  private formValue     : undefined | FormGroup<SigninFormScheme>;
 
+  public constructor(
+    @Inject(DOCUMENT)
+    private readonly document: Document,
+
+    private readonly fb   : FormBuilder,
+    private readonly route: ActivatedRoute,
+    private readonly sub  : Subscription,
+
+    public readonly vm: SigninViewModel) {}
+
+  public get form(): FormGroup<SigninFormScheme> {
+    return this.formValue ?? (this.formValue = this.buildForm());
+  }
+
+  public ngOnInit(): void {
+    this.sub.add(this.route.queryParamMap.subscribe(params => {
+      const returnUrl = params.get('returnUrl');
+
+      if (returnUrl) {
+        this.returnUrlValue = returnUrl;
+      }
+    }));
+
+    this.sub.add(this.form.valueChanges.subscribe(value => {
+      this.vm.email    = value.email    ?? '';
+      this.vm.password = value.password ?? '';
+    }));
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  public signin(): void {
+    if (this.form.valid) {
+      this.sub.add(
+        this.vm.signin().subscribe(() => this.redirectBack()));
+    }
+  }
+
+  private redirectBack(): void {
+    if (this.document.defaultView && this.returnUrlValue) {
+      this.document.defaultView.open(this.returnUrlValue, '_self');
+    }
+  }
+
+  private buildForm(): FormGroup<SigninFormScheme> {
+    return this.fb.group({
+      email   : this.fb.control('', Validators.required),
+      password: this.fb.control('', Validators.required),
+    });
+  }
 }
