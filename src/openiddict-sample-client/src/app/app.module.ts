fd33349120f7e5a5b2fb34e87b0be778af47e78d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-10 21:40:51 +0300

    add home component

--- src/openiddict-sample-client/src/app/app.module.ts
+++ src/openiddict-sample-client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule       } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent     } from './app.component';
 import { SigninComponent  } from './components';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
