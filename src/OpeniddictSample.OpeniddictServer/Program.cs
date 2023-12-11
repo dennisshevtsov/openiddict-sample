f13bb40476fb9e6f8d03600a71939922e3b7beb6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-12-11 22:05:12 +0300

    add cors

--- src/OpeniddictSample.OpeniddictServer/Program.cs
+++ src/OpeniddictSample.OpeniddictServer/Program.cs
@@ -8,9 +8,15 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<OpeniddictServerDbSettings>(builder.Configuration);
 builder.Services.SetUpOpenIddict();
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 WebApplication app = builder.Build();
 app.SetUpDb();
 app.UseRouting();
+app.UseCors(builder => builder.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("http://localhost:5005"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
